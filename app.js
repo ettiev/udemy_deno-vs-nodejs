54a809f0b607c3244fc93d3a60d93cb61a90a5a1
ettiev <126219891+ettiev@users.noreply.github.com>
2023-12-04 16:41:07 +0200

    Starting up servers in Node and Deno

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const fs = require("fs").promises;
+const http = require("http");
 
-const text = "This is a test - and it should be stored in a file!";
-
-fs.writeFile("node-message.txt", text).then(() => {
-    console.log("Wrote file!")    
+const server = http.createServer((req, res) => {
+    res.end("Hello world (from Node!)");
 });
+
+server.listen(3000);
+
+
+
+// -----------------------------------------------------------//
+
+// const fs = require("fs").promises;
+
+// const text = "This is a test - and it should be stored in a file!";
+
+// fs.writeFile("node-message.txt", text).then(() => {
+//     console.log("Wrote file!")    
+// });
--- app.ts
+++ app.ts
@@ -1,8 +1,17 @@
-const text = "This is a test - and it should be stored in a file!"
+Deno.serve(
+    { port: 3000, hostname: "0.0.0.0" },
+    (_req) => new Response("Hello, world")
+  );
 
-const encoder = new TextEncoder();
-const data = encoder.encode(text);
 
-Deno.writeFile('message.txt', data). then(() => {
-    console.log("Wrote to file!")
-})
+
+// -----------------------------------------------------------//
+
+// const text = "This is a test - and it should be stored in a file!"
+
+// const encoder = new TextEncoder();
+// const data = encoder.encode(text);
+
+// Deno.writeFile('message.txt', data). then(() => {
+//     console.log("Wrote to file!")
+// })
