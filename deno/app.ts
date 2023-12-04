9b7ec3f2098ac2968d3f752ecae0cbbda09508f4
ettiev <126219891+ettiev@users.noreply.github.com>
2023-12-04 19:10:26 +0200

    created basic Deno rest API routes
    create rest api vir Deno, with same routes as Node rest API.
    New Deno routes:
    Get route,
    Post route,
    Put route,
    Delete route

--- deno/app.ts
+++ deno/app.ts
@@ -3,11 +3,18 @@
 
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
+import todoRoutes from "./routes/routes.ts"
+
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+app.use(async (ctx, next) => {
+  ctx.response.body = "Middleware";
+  await next();
 });
+
+app.use(todoRoutes.routes());
+app.use(todoRoutes.allowedMethods());
+
 
 await app.listen({ port: 8000 });
 
