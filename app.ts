9fecaa2e52e45b4c438a08221b85418e349b04c1
ettiev <126219891+ettiev@users.noreply.github.com>
2023-12-04 13:27:20 +0200

    Write file - Deno and NodeJs equavalents
    Deno TypeScript code used to write a file
    NodeJs JS code used to write a file

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
-let message: string
+const text = "This is a test - and it should be stored in a file!"
 
+const encoder = new TextEncoder();
+const data = encoder.encode(text);
 
-message = "Hi there!!"
-
-console.log(message);
+Deno.writeFile('message.txt', data). then(() => {
+    console.log("Wrote to file!")
+})
